c5e10dda7a12ee6a052ac864dcd316dab498b1a7
Valentin Burillier <valentin.burillier@gmail.com>
2023-12-17 13:05:39 +0100

    refont css 320*568px

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
-import {getSolPrice} from './scripts/priceSol.js';
-getSolPrice();
+function getDefautPriceSolana() {
+  const spanPrice = document.querySelector('#choiceCrypto');
+  const xhr = new XMLHttpRequest();
+  const iconSolana = document.querySelector('.iconSolana');
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      if (xhr.status === 200) {
+        let jsonSolanaRequest = JSON.parse(xhr.response);
+        let priceSolana = jsonSolanaRequest.market_data.current_price.usd;
+        spanPrice.innerText = priceSolana;
+        iconSolana.style.borderBottom = "3px solid gold";
+      } else {
+        console.log('Erreur lors de la requête');
+      }
+    }
+  };
+  
+  xhr.open('GET', 'https://api.coingecko.com/api/v3/coins/solana?');
+  xhr.send();
+  
+}
+getDefautPriceSolana();
